550746ce33cad4a1b60c158e30fa4bc0d0c49258
Tom Lamb <lambtsa.tom@gmail.com>
2023-08-09 20:58:04 +0200

    Update to next 13

--- next.config.js
+++ next.config.js
@@ -1,29 +1,36 @@
-module.exports = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    // const configPath = path.resolve(
-    //   "src/config/env",
-    //   `${process.env.BUILD_ENV}.json`
-    // );
-    // Enable <React.StrictMode> in application
-    reactStrictMode: true,
-    env: {
-      MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
-      gaTrackingId: process.env.gaTrackingId,
+const isLocalLikeBuildEnv =
+  !process.env.BUILD_ENV || process.env.BUILD_ENV === "local";
+const isProductionLikeRuntime = process.env.NODE_ENV === "production";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // Enable <React.StrictMode> in application
+  reactStrictMode: true,
+  env: {
+    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
+    gaTrackingId: process.env.gaTrackingId,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      pure: isProductionLikeRuntime,
+      minify: isProductionLikeRuntime,
+      transpileTemplateLiterals: isProductionLikeRuntime,
+      fileName: isLocalLikeBuildEnv,
+      displayName: isLocalLikeBuildEnv,
     },
-
-    // fileExtensions: ["jpg", "jpeg", "png", "gif"],
-
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"],
-      });
-
-      return config;
-    },
-  };
-  return nextConfig;
+  },
 };
+
+module.exports = nextConfig;
