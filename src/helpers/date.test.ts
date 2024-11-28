a00322f51b5a8f67235db65fa1fc65236d57f38c
lambtsa-pro <tom.lamb@decathlon.com>
2024-11-28 20:42:57 +0100

    feat: update dates and steps

--- src/helpers/date.test.ts
+++ src/helpers/date.test.ts
@@ -2,12 +2,12 @@ import { dateToString, formatTime } from "./date";
 
 describe("Test formatTime", () => {
   test("returns correct hours", () => {
-    const now = new Date("2023-08-05T06:18:29.418Z");
-    expect(formatTime(now)).toBe("2023-08-05T08:18");
+    const now = new Date("2023-08-05T06:18");
+    expect(formatTime(now)).toBe("2023-08-05T06:30");
   });
   test("returns correct minutes", () => {
-    const now = new Date("2023-08-05T18:08:29.418Z");
-    expect(formatTime(now)).toBe("2023-08-05T20:08");
+    const now = new Date("2023-08-05T18:08");
+    expect(formatTime(now)).toBe("2023-08-05T18:30");
   });
 });
 
@@ -17,7 +17,7 @@ describe("Test dateToString", () => {
     expect(dateToString(now)).toBe("samedi 5 août 2023 06:18");
   });
   test("returns correct minutes", () => {
-    const now = new Date("2023-08-05T18:08:29.418Z");
-    expect(dateToString(now)).toBe("samedi 5 août 2023 20:08");
+    const now = new Date("2023-08-05T19:18");
+    expect(dateToString(now)).toBe("samedi 5 août 2023 19:18");
   });
 });
--- typings/global.d.ts
+++ typings/global.d.ts
@@ -3,7 +3,7 @@ declare namespace NodeJS {
    * Add custom environment variables
    */
   interface ProcessEnv {
-    readonly gaTrackingId: string;
+    readonly GTAG: string;
     readonly MAPBOX_TOKEN: string;
     readonly ZOHO_ID: string;
     readonly ZOHO_PASSWORD: string;
